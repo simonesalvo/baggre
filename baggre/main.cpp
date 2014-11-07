4a2cc93b0e5ec400e1e82e2a49aff9d639bbbafe
Simone Salvo <simone.salvo@ymail.com>
2014-11-07 14:08:38 +0100

    Initial Commit

---
+++ baggre/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  baggre
+//
+//  Created by Simone Salvo on 07/11/14.
+//  Copyright (c) 2014 Simone Salvo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
