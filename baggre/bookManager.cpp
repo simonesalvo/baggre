d92f8c7384dd25f7831634dbe37d6a95aac646a2
Simone Salvo <simone.salvo@ymail.com>
2014-11-20 12:24:34 +0100

    * Details

---
+++ baggre/bookManager.cpp
@@ -0,0 +1,9 @@
+//
+//  bookManager.cpp
+//  baggre
+//
+//  Created by Simone Salvo on 20/11/14.
+//  Copyright (c) 2014 Simone Salvo. All rights reserved.
+//
+
+#include "bookManager.h"
---
+++ baggre/bookManager.h
@@ -0,0 +1,14 @@
+//
+//  bookManager.h
+//  baggre
+//
+//  Created by Simone Salvo on 20/11/14.
+//  Copyright (c) 2014 Simone Salvo. All rights reserved.
+//
+
+#ifndef __baggre__bookManager__
+#define __baggre__bookManager__
+
+#include <stdio.h>
+
+#endif /* defined(__baggre__bookManager__) */
