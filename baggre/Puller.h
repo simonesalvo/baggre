aa24e467d32e8fe1b30c314a7e17f95f147fa4b9
Simone Salvo <simone.salvo@ymail.com>
2014-11-20 10:41:35 +0100

    * Added new feature. Core building phase.

--- baggre/Puller.h
+++ baggre/Puller.h
@@ -7,9 +7,39 @@
 //
 
 #include <curl/curl.h>
+#include <curl/easy.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <stdio.h>
 
 #ifndef baggre_Puller_h
 #define baggre_Puller_h
+class Puller
+{
+    // Attribute
+public:
+    
+    // Attribute
+private:
+    CURL * curl;
+    std::string path;
+    std::string filename;
+    
+    // Function
+public:
+     Puller();
+    ~Puller();
 
-
+    bool getPage(std::vector<std::string> url);
+    std::string getPath();
+    std::string getFilename();
+    std::string setFilename();
+    void setPath(std::string);
+    
+    
+    // Function
+private:
+    
+};
 #endif
