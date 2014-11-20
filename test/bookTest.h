24455d5883413c63ea699042ce4b18b08349749c
Simone Salvo <simone.salvo@ymail.com>
2014-11-20 20:07:25 +0100

    * Test architecture

---
+++ test/bookTest.h
@@ -0,0 +1,14 @@
+//
+//  bookTest.h
+//  baggre
+//
+//  Created by Simone Salvo on 20/11/14.
+//  Copyright (c) 2014 Simone Salvo. All rights reserved.
+//
+
+#ifndef __baggre__bookTest__
+#define __baggre__bookTest__
+
+#include <stdio.h>
+
+#endif /* defined(__baggre__bookTest__) */
