9e1073d3ad5e8ff9c9344e6f8603c315907824ff
Simone Salvo <simone.salvo@ymail.com>
2014-11-20 10:41:06 +0100

    * Added new feature. Core building phase.

---
+++ baggre/cloudManager.h
@@ -0,0 +1,25 @@
+//
+//  cloudManager.h
+//  baggre
+//
+//  Created by Simone Salvo on 13/11/14.
+//  Copyright (c) 2014 Simone Salvo. All rights reserved.
+//
+
+#ifndef __baggre__cloudManager__
+#define __baggre__cloudManager__
+
+#include <string>
+
+class cloudManager
+{
+private:
+private:
+public:
+public:
+    cloudManager();
+    ~cloudManager();
+
+    
+};
+#endif /* defined(__baggre__cloudManager__) */
